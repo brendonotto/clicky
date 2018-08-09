0891a3057c503ca61029a1aab84e3da2dfb6060b
Brendon Otto <brendon@brendonotto.com>
2018-08-08 23:43:48 -0500

    Initial project set up

---
+++ fw/config/config.exs
@@ -0,0 +1,49 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# Customize non-Elixir parts of the firmware.  See
+# https://hexdocs.pm/nerves/advanced-configuration.html for details.
+config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
+
+# Use shoehorn to start the main application. See the shoehorn
+# docs for separating out critical OTP applications such as those
+# involved with firmware updates.
+config :shoehorn,
+  init: [:nerves_runtime, :nerves_network],
+  app: Mix.Project.config()[:app]
+
+# Import target specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+# Uncomment to use target specific configurations
+
+# import_config "#{Mix.Project.config[:target]}.exs"
+
+# For WiFi, set regulatory domain to avoid restrictive default
+config :nerves_network,
+  regulatory_domain: "US"
+
+config :nerves_network, :default,
+  wlan0: [
+    ssid: System.get_env("NERVES_NETWORK_SSID"),
+    psk: System.get_env("NERVES_NETWORK_PSK"),
+    key_mgmt: String.to_atom(System.get_env("NERVES_NETWORK_MGMT"))
+  ],
+  eth0: [
+    ipv4_address_method: :dhcp
+  ]
+
+config :ui, UiWeb.Endpoint,
+  url: [host: "localhost"],
+  http: [port: 80],
+  secret_key_base: "erNaFo3KLCaes+h6aPaimgLEoOcNTuqdZnPKIllNA4sMU896PNLkJBInDi2T258/",
+  root: Path.dirname(__DIR__),
+  server: true,
+  render_errors: [view: UiWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Nerves.PubSub, adapter: Phoenix.PubSub.PG2],
+  code_reloader: false
+
+config :logger, level: :debug
