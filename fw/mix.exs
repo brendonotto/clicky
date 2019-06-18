70026eef8ab4960587f7a5107548d8eca8546b72
Brendon Otto <brendon@brendonotto.com>
2019-06-17 23:24:36 -0500

    Updating dependencies to newer version, still broke though :(

--- fw/mix.exs
+++ fw/mix.exs
@@ -7,9 +7,9 @@ defmodule Fw.MixProject do
     [
       app: :fw,
       version: "0.1.0",
-      elixir: "~> 1.4",
+      elixir: "~> 1.8",
       target: @target,
-      archives: [nerves_bootstrap: "~> 1.0"],
+      archives: [nerves_bootstrap: "~> 1.5"],
       deps_path: "deps/#{@target}",
       build_path: "_build/#{@target}",
       lockfile: "mix.lock.#{@target}",
@@ -19,12 +19,16 @@ defmodule Fw.MixProject do
     ]
   end
 
+  IO.puts("Passed Project")
+
   # Starting nerves_bootstrap adds the required aliases to Mix.Project.config()
   # Aliases are only added if MIX_TARGET is set.
   def bootstrap(args) do
     Application.start(:nerves_bootstrap)
     Mix.Task.run("loadconfig", args)
   end
+
+  IO.puts("Passed bootstrap")
 
   # Run "mix help compile.app" to learn about applications.
   def application do
@@ -33,34 +37,42 @@ defmodule Fw.MixProject do
       extra_applications: [:logger, :runtime_tools]
     ]
   end
+  IO.puts("Passed application")
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nerves, "~> 1.3", runtime: false},
-      {:nerves_network, "~> 0.3"},
+      {:nerves, "~> 1.4", runtime: false},
+      {:nerves_network, "~> 0.5"},
       {:ui, path: "../ui"},
-      {:shoehorn, "~> 0.2"}
+      {:shoehorn, "~> 0.4"},
+      {:ring_logger, "~> 0.6"},
+      {:toolshed, "~> 0.2"},
     ] ++ deps(@target)
   end
+
+  IO.puts("Passed first deps")
 
   # Specify target specific dependencies
   defp deps("host"), do: []
 
   defp deps(target) do
     [
-      {:nerves_runtime, "~> 0.8"}
+      {:nerves_runtime, "~> 0.6"},
+      {:nerves_init_gadget, "~> 0.4"},
     ] ++ system(target)
   end
 
-  defp system("rpi"), do: [{:nerves_system_rpi, "~> 1.0", runtime: false}]
-  defp system("rpi0"), do: [{:nerves_system_rpi0, "~> 1.0", runtime: false}]
+  IO.puts("Passed deps(target")
+
+  defp system("rpi"), do: [{:nerves_system_rpi, "~> 1.6", runtime: false}]
+  defp system("rpi0"), do: [{:nerves_system_rpi0, "~> 1.6", runtime: false}]
   defp system("rpi0_wiringPi"), do: [{:nerves_system_rpi0_wiringPi, path: "../nerves_system_rpi0_wiringPi", runtime: false}]
-  defp system("rpi2"), do: [{:nerves_system_rpi2, "~> 1.0", runtime: false}]
-  defp system("rpi3"), do: [{:nerves_system_rpi3, "~> 1.0", runtime: false}]
-  defp system("bbb"), do: [{:nerves_system_bbb, "~> 1.0", runtime: false}]
-  defp system("ev3"), do: [{:nerves_system_ev3, "~> 1.0", runtime: false}]
-  defp system("qemu_arm"), do: [{:nerves_system_qemu_arm, "~> 1.0", runtime: false}]
-  defp system("x86_64"), do: [{:nerves_system_x86_64, "~> 1.0", runtime: false}]
+  defp system("rpi2"), do: [{:nerves_system_rpi2, "~> 1.6", runtime: false}]
+  defp system("rpi3"), do: [{:nerves_system_rpi3, "~> 1.6", runtime: false}]
+  defp system("bbb"), do: [{:nerves_system_bbb, "~> 2.0", runtime: false}]
+  defp system("x86_64"), do: [{:nerves_system_x86_64, "~> 1.6", runtime: false}]
   defp system(target), do: Mix.raise("Unknown MIX_TARGET: #{target}")
+
+  IO.puts("Passed system deps")
 end
